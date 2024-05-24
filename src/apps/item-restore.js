ab652a08ef0941cfa875b36fd46b8ea276ebb74d
ddipaolo <ddipaolo@gmail.com>
2024-05-23 23:39:24 -0500

    refactor diff into more data driven func and with a sparser return

--- src/apps/item-restore.js
+++ src/apps/item-restore.js
@@ -1,5 +1,95 @@
 import { Constants } from "../constants.js";
 
+const sectionFields = [
+    {
+        "section": "General",
+        "fields": [
+            { "field": "name", "label": "Name" },
+            { "field": "img", "label": "Image" },
+            { "field": "system.equipped", "label": "Equipped?" },
+            { "field": "system.identified", "label": "Identified?" },
+            { "field": "system.rarity", "label": "Rarity" },
+            { "field": "system.quantity", "label": "Quantity" },
+            { "field": "system.weight", "label": "Weight" },
+            { "field": "system.price.value", "label": "Price (value" },
+            { "field": "system.price.denomination", "label": "Price (denomination)" },
+        ]
+    },
+    {
+        "section": "Description",
+        "fields": [
+            // { "field": "system.unidentified.name", "label": "Unidentified Name" },
+            // { "field": "system.unidentified.description", "label": "Unidentified Description" },
+            { "field": "system.description.value", "label": "Description" },
+            { "field": "system.description.chat", "label": "Chat Description" },
+        ]
+    },
+    {
+        "section": "Details",
+        "sections": [
+            {
+                "section": "General",
+                "fields": [
+                    { "field": "system.type.value", "label": "Type" },
+                    { "field": "system.type.baseItem", "label": "Base Item" },
+                    // { "field": "system.type.label", "label": "Item Label" },
+                    { "field": "system.attunement", "label": "Attunement", "renderFunc": renderAttunement },
+                    //"system.proficient",
+                    //"system.properties",   // need to figure this out perhaps
+                    { "field": "system.magicalBonus", "label": "Magical Bonus" },
+                    { "field": "system.armor.value", "label": "Armor Class" },
+                    { "field": "system.armor.magicalBonus", "label": "Armor Magic Bonus" },
+                    { "field": "system.armor.dex", "label": "Max Dexterity Modifier" },
+                    { "field": "system.strength", "label": "Required Strength" },
+                ]
+            },
+            {
+                "section": "Usage",
+                "fields": [
+                    { "field": "system.activation.type", "label": "Activation Cost (units)" },
+                    { "field": "system.activation.cost", "label": "Activation Cost (#)" },
+                    { "field": "system.activation.condition", "label": "Activation Condition" },
+                    { "field": "system.target.type", "label": "Target (type)" },
+                    { "field": "system.target.value", "label": "Target (#)" },
+                    { "field": "system.target.units", "label": "Target (units)" },
+                    { "field": "system.range.units", "label": "Range (units)" },
+                    { "field": "system.range.value", "label": "Range (normal)" },
+                    { "field": "system.range.long", "label": "Range (long)" },
+                    { "field": "system.duration.units", "label": "Duration (units)" },
+                    { "field": "system.duration.value", "label": "Duration (#)" },
+                    { "field": "system.uses.per", "label": "Limited Uses (units)" },
+                    { "field": "system.uses.value", "label": "Limited Uses (#)" },
+                    { "field": "system.uses.max", "label": "Limited Uses (Max)" },
+                    { "field": "system.uses.recovery", "label": "Recovery Formula" },
+                    { "field": "system.uses.autoDestroy", "label": "Destroy on Empty" },
+                    { "field": "system.consume.type", "label": "Resource Consumption (units)" },
+                    { "field": "system.consume.target", "label": "Resource Consumption (target)" },
+                    { "field": "system.consume.amount", "label": "Resource Consumption (#)" },
+                    { "field": "system.consume.scales", "label": "Scales" },  //??
+                ]
+            },
+            {
+                "section": "Action",
+                "fields": [
+                    { "field": "system.actionType", "label": "Action Type" },
+                    { "field": "system.ability", "label": "Ability Modifier" },
+                    { "field": "system.attack.bonus", "label": "Attack Roll Bonus" },
+                    { "field": "system.attack.flat", "label": "Flat Bonus" },
+                    { "field": "system.critical.threshold", "label": "Critical Hit Threshold" },
+                    { "field": "system.critical.damage", "label": "Extra Critical Hit Damage" },
+                    //"system.damage.parts" // need to do?
+                    { "field": "system.damage.versatile", "label": "Versatile Damage" },
+                    { "field": "system.formula", "label": "Other Formula" },
+                    { "field": "system.save.ability", "label": "Saving Throw" },
+                    { "field": "system.save.dc", "label": "Saving Throw DC" },
+                    { "field": "system.save.scaling", "label": "Saving Throw Target" },
+                    { "field": "system.chatFlavor", "label": "Chat Message Flavor" },
+                ]
+            }
+        ]
+    }
+];
+
 function fetchFromObject(obj, prop) {
     if (typeof obj === 'undefined') {
         return false;
@@ -19,140 +109,42 @@ function fetchFromObject(obj, prop) {
     return obj[prop];
 }
 
-/**
- * Compares two Item5e's for a specific set of different fields and produces an
- * object with similar field structure to an Item5e that notes which fields are
- * different between the two.
- * 
- * @param {Item5e} itemBeingReplaced 
- * @param {Item5e} itemReplacing 
- * @returns {object} // a diff object that is true if the two objects are the same at the same property
- */
-function Item5eCompare(itemBeingReplaced, itemReplacing) {
-    // we only are specifically looking for some attributes
-    diffObject = {
-        "system": {}
-    }
-    
-    diffObject.name = itemBeingReplaced.name == itemReplacing.name;
-    diffObject.img = itemBeingReplaced.img == itemReplacing.img;
-    diffObject.system.equipped = itemBeingReplaced.system?.equipped == itemReplacing.system.equipped;
-    diffObject.system.identified = itemBeingReplaced.system?.identified == itemReplacing.system?.identified;
-    diffObject.system.rarity = itemBeingReplaced.system?.rarity == itemReplacing.system?.rarity;
-
-    // Description Tab
-    diffObject.system.quantity = itemBeingReplaced.system?.quantity == itemReplacing.system?.quantity;
-    diffObject.system.weight = itemBeingReplaced.system?.weight == itemReplacing.system?.weight;
-    if (itemBeingReplaced.system?.price?.value != null || itemReplacing.system?.price?.value != null) {
-        diffObject.system.price = {};
-        diffObject.system.price.value = itemBeingReplaced.system?.price?.value == itemReplacing.system?.price?.value;
-        diffObject.system.price.denomination = itemBeingReplaced.system?.price?.denomination == itemReplacing.system?.price?.denomination;
-    }
-
-    if (itemBeingReplaced.system?.unidentified?.name != null || itemReplacing.system?.unidentified?.name != null) {
-        diffObject.system.unidentified = {};
-        diffObject.system.unidentified.name = itemBeingReplaced.system?.unidentified?.name == itemReplacing.system?.unidentified?.name;
-        diffObject.system.unidentified.description = itemBeingReplaced.system?.unidentified?.description == itemReplacing.system?.unidentified?.description;
-    }
-    if (itemBeingReplaced.system?.description?.value != null || itemReplacing.system?.description?.value != null) {
-        diffObject.system.description = {};
-        diffObject.system.description.value = itemBeingReplaced.system?.description?.value == itemReplacing.system?.description?.value;
-        diffObject.system.description.chat = itemBeingReplaced.system?.description?.chat == itemReplacing?.system?.description?.chat;
-    }
-
-    // Details Tab
-    if (itemBeingReplaced.system?.type?.value != null || itemReplacing.system?.type?.value != null) {
-        diffObject.system.type = {};
-        diffObject.system.type.value = itemBeingReplaced.system?.type?.value == itemReplacing.system?.type?.value;
-        diffObject.system.type.baseItem = itemBeingReplaced.system?.type?.baseItem == itemReplacing.system?.type?.baseItem;
-        diffObject.system.type.label = itemBeingReplaced.system?.type?.label == itemReplacing.system?.type?.label; // may be auto-populated, may want to remove
-    }
-
-    diffObject.system.attunement = itemBeingReplaced.system?.attunement == itemReplacing.system?.attunement;
-    diffObject.system.proficient = itemBeingReplaced.system?.proficient == itemReplacing.system?.proficient;
-    diffObject.system.properties = itemBeingReplaced.system?.properties == itemReplacing.system?.properties;
-
-    diffObject.system.magicalBonus = itemBeingReplaced.system?.magicalBonus == itemReplacing.system?.magicalBonus;
-    if (itemBeingReplaced.system?.armor?.value != null || itemReplacing.system?.armor?.value != null) {
-        diffObject.system.armor = {};
-        diffObject.system.armor.value = itemBeingReplaced.system?.armor?.value == itemReplacing.system?.armor?.value;
-        diffObject.system.armor.magicalBonus = itemBeingReplaced.system?.armor?.magicalBonus == itemReplacing.system?.armor?.magicalBonus;
-        diffObject.system.armor.dex = itemBeingReplaced.system?.armor?.dex == itemReplacing.system?.armor?.dex;
-    }
-    diffObject.system.strength = itemBeingReplaced.system?.strength == itemReplacing.system?.strength;
-
-    // can skip Usage section entirely if neither have it
-    if (itemBeingReplaced.system?.activation?.type != null || itemReplacing.system?.activation?.type != null) {
-        // one of them has an activation type so we have to check these
-        diffObject.system.activation = {};
-        diffObject.system.activation.type = itemBeingReplaced.system?.activation?.type == itemReplacing.system?.activation?.type;
-        diffObject.system.activation.cost = itemBeingReplaced.system?.activation?.cost == itemReplacing.system?.activation?.cost;
-        diffObject.system.activation.condition = itemBeingReplaced.system?.activation?.condition == itemReplacing.system?.activation?.condition;
-
-        if (itemBeingReplaced.system?.target?.type != null || itemReplacing.system?.target?.type != null) {
-            diffObject.system.target = {};
-            diffObject.system.target.type = itemBeingReplaced.system?.target?.type == itemReplacing.sytem?.target?.type;
-            diffObject.system.target.value = itemBeingReplaced.system?.target?.value == itemReplacing.sytem?.target?.value;
-            diffObject.system.target.units = itemBeingReplaced.system?.target?.units == itemReplacing.sytem?.target?.units;
-        }
-        if (itemBeingReplaced.system?.range?.units != null || itemReplacing.system?.range?.units != null) {
-            diffObject.system.range = {};
-            diffObject.system.range.units = itemBeingReplaced.system?.range?.units == itemReplacing.system?.range?.units;
-            diffObject.system.range.value = itemBeingReplaced.system?.range?.value == itemReplacing.system?.range?.value;
-            diffObject.system.range.long = itemBeingReplaced.system?.range?.long == itemReplacing.system?.range?.long;
-        }
-        if (itemBeingReplaced.system?.duration?.units != null || itemReplacing.system?.duration?.units != null) {
-            diffObject.system.duration = {};
-            diffObject.system.duration.units = itemBeingReplaced.system?.duration?.units == itemReplacing.system?.duration?.units;
-            diffObject.system.duration.value = itemBeingReplaced.system?.duration?.value == itemReplacing.system?.duration?.value;
-        }
-        if (itemBeingReplaced.system?.uses?.per != null || itemReplacing.system?.uses?.per != null) {
-            diffObject.system.uses = {};
-            diffObject.system.uses.per = itemBeingReplaced.system?.uses?.per == itemReplacing.system?.uses?.per;
-            diffObject.system.uses.value = itemBeingReplaced.system?.uses?.value == itemReplacing.system?.uses?.value;
-            diffObject.system.uses.max = itemBeingReplaced.system?.uses?.max == itemReplacing.system?.uses?.max;
-            diffObject.system.uses.recovery = itemBeingReplaced.system?.uses?.recovery == itemReplacing.system?.uses?.recovery;
-            diffObject.system.uses.autoDestroy = itemBeingReplaced.system?.uses?.autoDestroy == itemReplacing.system?.uses?.autoDestroy;
-        }
-        if (itemBeingReplaced.system?.consume?.type != null || itemReplacing.system?.consume?.type != null) {
-            diffObject.system.consume = {};
-            diffObject.system.consume.type = itemBeingReplaced.system?.consume?.type == itemBeingReplaced.system?.consume?.type;
-            diffObject.system.consume.target = itemBeingReplaced.system?.consume?.target == itemBeingReplaced.system?.consume?.target;
-            diffObject.system.consume.amount = itemBeingReplaced.system?.consume?.amount == itemBeingReplaced.system?.consume?.amount; // this should always differ if it's set (with normal use cases)
-            diffObject.system.consume.scales = itemBeingReplaced.system?.consume?.scales == itemBeingReplaced.system?.consume?.scales;
+function getDiffFields(existingItem, referenceItem, sectionFields) {
+    // pseudocode:
+    // iterate over sections in field list:
+    //   if section has its own sections:
+    //     recurse, and assign results to this section
+    //   else:
+    //     for each field in section fields:
+    //       get existing
+    //       get reference
+    //       if either is non-null:
+    //         create object with both values with key of field name
+    let diffList = []
+    for (const sectionDefinition of sectionFields) {
+        if (sectionDefinition.hasOwnProperty('sections')) {
+            diffObject[sectionDefinition['section']] = getDiffFields(existingItem, referenceItem, sectionDefinition['sections'])
+        } else {
+            let fieldDiffs = [];
+            for (const fieldEntry of sectionDefinition['fields']) {
+                let existingItemValue = fetchFromObject(existingItem, fieldEntry['field']);
+                let referenceItemValue = fetchFromObject(referenceItem, fieldEntry['field']);
+                if (existingItemValue != null && referenceItemValue != null &&
+                    existingItemValue != undefined && referenceItemValue != undefined &&
+                    existingItemValue != referenceItemValue) {
+                        if ('renderFunc' in fieldEntry) {
+                            let renderFunc = fieldEntry['renderFunc'];
+                            fieldDiffs.push({ "field": fieldEntry['field'], "label": fieldEntry['label'], "existing": renderFunc(existingItemValue), "reference": renderFunc(referenceItemValue) });
+                        } else {
+                            fieldDiffs.push({ "field": fieldEntry['field'], "label": fieldEntry['label'], "existing": existingItemValue, "reference": referenceItemValue });
+                        }
+                }
+            }
+            let sectionDiff = { "section": sectionDefinition["section"], "fields": fieldDiffs };
+            diffList.push(sectionDiff);
         }
     }
-    // can skip Action/Attack section entirely if neither have it
-    if (itemBeingReplaced.system.actionType != "" || itemReplacing.system.actionType != "") {
-        // one of them has an action Type so we have to check these
-        diffObject.system.actionType = itemBeingReplaced.system?.actionType == itemReplacing.system?.actionType;
-        diffObject.system.ability = itemBeingReplaced.system?.ability == itemReplacing.system?.ability;
-        if (itemBeingReplaced.system?.attack != null || itemReplacing.system?.attack != null) {
-            diffObject.system.attack = {};
-            diffObject.system.attack.bonus = itemBeingReplaced.system?.attack?.bonus == itemReplacing.system?.attack?.bonus;
-            diffObject.system.attack.flat = itemBeingReplaced.system?.attack?.flat == itemReplacing.system?.attack?.flat;
-        }
-        if (itemBeingReplaced.system?.critical != null || itemReplacing.system?.critical != null) {
-            diffObject.system.critical = {};
-            diffObject.system.critical.threshold = itemBeingReplaced.system?.critical?.threshold == itemReplacing.system?.critical?.threshold;
-            diffObject.system.critical.damage = itemBeingReplaced.system?.critical?.damage == itemReplacing.system?.critical?.damage;
-        }
-        if (itemBeingReplaced.system?.damage != null || itemReplacing.system?.damage != null) {
-            diffObject.system.damage = {};
-            // TODO(dd): damage parts needs to be handled differently
-            diffObject.system.damage.versatile = itemBeingReplaced.system?.damage?.versatile == itemReplacing.system?.damage?.versatile;
-        }
-        diffObject.system.formula = itemBeingReplaced.system?.formula == itemReplacing.system?.formula;
-        if (itemBeingReplaced.system?.save?.ability != null || itemReplacing.system?.save?.ability != null) {
-            diffObject.system.save = {};
-            diffObject.system.save.ability = itemBeingReplaced.system?.save?.ability == itemReplacing.system?.save?.ability;
-            diffObject.system.save.dc = itemBeingReplaced.system?.save?.dc == itemReplacing.system?.save?.dc;
-            diffObject.system.save.scaling = itemBeingReplaced.system?.save?.scaling == itemReplacing.system?.save?.scaling;
-        }
-        diffObject.system.chatFlavor = itemBeingReplaced.system?.chatFlavor == itemReplacing.system?.chatFlavor;
-    }
-
-    return diffObject;
+    return diffList;
 }
 
 function renderBaseType(itemType) {
@@ -245,7 +237,8 @@ export class ItemRestoreApp extends FormApplication {
         this.actor = game.actors.get(this.actorId);
         this.existingItem = this.actor.items.find(x => x._id == this.itemId);
         this.originalItem = await fromUuid(origItemId);
-        this.diff = Item5eCompare(this.existingItem, this.originalItem);
+        this.diff = getDiffFields(this.existingItem, this.originalItem, sectionFields);
+        debugger;
     }
 
     async _resetItem() {
