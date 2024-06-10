490e73bde3f78ae9f84387a6b1d08c41109c50e1
ddipaolo <ddipaolo@gmail.com>
2024-06-10 12:44:08 -0500

    use null derefs in exhaustion handler

--- src/exhaustion.js
+++ src/exhaustion.js
@@ -1,7 +1,7 @@
 export async function ExhaustionDamageHandler(actor, heal, diff, id) { 
     // if current HP equals the heal value, we were at zero, so we
     // up the exhaustion by one
-    if (diff.system.attributes.hp.value == 0) {
+    if (diff?.system?.attributes?.hp?.value == 0) {
         console.log("materia-dnd | Exhaustion: " + actor.name + " has dropped to 0 HP, upping exhaustion by 1");
         let newExhLevel = actor.system.attributes.exhaustion + 1;
         actor.update({ "system.attributes.exhaustion": newExhLevel });
@@ -14,11 +14,11 @@ export async function ExhaustionDamageHandler(actor, heal, diff, id) {
 
 export function ExhastionActiveEffectHandler(activeEffect, flags, diff, id) {
     let actor = activeEffect.parent;
-    if (flags.flags.dnd5e.exhaustionLevel == 3) {
+    if (flags?.flags?.dnd5e?.exhaustionLevel == 3) {
         // special case, dropping from 4 to 3 removes the vulnerability we imposed
         actor.system.traits.dv.value.clear();
     }
-    if (flags.flags.dnd5e.exhaustionLevel >= 4) {
+    if (flags?.flags?.dnd5e?.exhaustionLevel >= 4) {
         actor.system.traits.dv.value.clear();
         makeVulnerableToAllDamageTypes(actor);
     }
