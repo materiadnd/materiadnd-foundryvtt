a3d83953c76b7c0655a12f329390a6d3e20e8275
ddipaolo <ddipaolo@gmail.com>
2024-07-06 03:47:37 -0500

    snapshot:  made selectors mutually exclusive, other minor cleanups and wireup

--- src/apps/stat-roller.js
+++ src/apps/stat-roller.js
@@ -1,4 +1,4 @@
-import { Constants } from "../constants.js";
+import { ButtonFlags, Constants } from "../constants.js";
 
 export function StatRollerRenderActorSheetHandler(app, html, actor) {
     // only display for PC sheets
@@ -23,8 +23,7 @@ export function StatRollerRenderActorSheetHandler(app, html, actor) {
         const buttonText = game.i18n.localize('MATERIA-DND.ui.stat-roller.charsheet-titlebar-button');
         let openButton = $(`<a class="open-stat-roller"><i class="fa-solid fa-dice"></i> ${buttonText}</a>`);
         openButton.on("click", async (event) => {
-            var statRollerApp = new StatRollerApp();
-            statRollerApp.setActor(actor._id);
+            var statRollerApp = new StatRollerApp(actor._id);
             statRollerApp.render(true);
         });
         html.closest('.app').find('.open-stat-roller').remove();
@@ -34,8 +33,9 @@ export function StatRollerRenderActorSheetHandler(app, html, actor) {
 }
 
 class StatRollerApp extends Application {
-    constructor() {
+    constructor(actorId) {
         super();
+        this.actorId = actorId;
         this._initialize();
     }
 
@@ -55,36 +55,84 @@ class StatRollerApp extends Application {
 
     getData() {
         let data = {};
+        data.buttonStates = this.buttonStates;
         data.discardedRoll = this.discardedRoll;
         data.rolls = this.rolls;
         return data;
     }
 
     activateListeners(html) {
-        html.find("#roller-roll-starting-stats-button").on("click", ev => this._rollStartingStats());
-    }
+        html.find(".roll-starting-stats-button").on("click", ev => this._rollStartingStats());
+        html.find(".assign-stats-button").on("click", ev => this._assignStatsAndClose(html));
 
-    setActor(actorId) {
-        this.actorId = actorId;
+        html.find(".stat-selector").change( ev => this._onStatSelectorChange(ev, html.find('.stat-selector')));
     }
 
     _initialize() {
         loadTemplates({
             rollDisplay: Constants.TEMPLATES.STAT_ROLLER.ROLL_DISPLAY
         });
+        this.buttonStates = {
+            'roll-starting-stats-button': ButtonFlags.Enabled,
+            'set-stat-to-eight-button': ButtonFlags.Disabled,
+            'reroll-one-stat-button': ButtonFlags.Disabled,
+            'start-over-button': ButtonFlags.Disabled,
+            'assign-stats-button': ButtonFlags.Disabled,
+        }
+        Handlebars.registerHelper('isEnabled', function(btnName) {
+            if (btnName in this.buttonStates) {
+                if (this.buttonStates[btnName] === ButtonFlags.Enabled) {
+                    return "enabled";
+                } else if (this.buttonStates[btnName] === ButtonFlags.Disabled) {
+                    return "disabled";
+                } else {
+                    return "";
+                }
+            } else {
+                return "";
+            }
+        });
     }
 
     async _rollStartingStats() {
-        let candidateRolls = [];
-        for (let i=1; i <= 7; i++) {
-            let roll = new Roll("4d6kh3");
-            await roll.evaluate();
-            console.log(`materia-dnd | Stat Roller: Roll ${i} = ${roll.total}`);
-            candidateRolls.push(roll);
+        if (this.buttonStates['roll-starting-stats-button'] == ButtonFlags.Enabled) {
+            let candidateRolls = [];
+            for (let i=1; i <= 7; i++) {
+                let roll = new Roll("4d6kh3");
+                await roll.evaluate();
+                console.log(`materia-dnd | Stat Roller: Roll ${i} = ${roll.total}`);
+                candidateRolls.push(roll);
+            }
+            candidateRolls.sort((a, b) => a.total - b.total);
+            this.discardedRoll = candidateRolls.shift();
+            this.rolls = candidateRolls;
+            // now we assess what the next state is
+            this.buttonStates['roll-starting-stats-button'] = ButtonFlags.Disabled;  // cannot roll new stats
+            this.buttonStates['set-stat-to-eight-button'] = ButtonFlags.Enabled;     // can always set something to eight
+            // if the stats include a negative modifier already, can assign stats
+            if (candidateRolls[0].total < 10) {
+                this.buttonStates['assign-stats-button'] = ButtonFlags.Enabled;
+            } else {
+                this.buttonStates['assign-stats-button'] = ButtonFlags.Disabled;
+            }
+            this.render(true);
         }
-        candidateRolls.sort((a, b) => a.total - b.total);
-        this.discardedRoll = candidateRolls.shift();
-        this.rolls = candidateRolls;
-        this.render(true);
     }
+
+    _onStatSelectorChange(ev, statSelectors) {
+        let selectorId = ev.target.id;
+        let selectedStat = ev.target.value;
+        for (var i = 0; i < statSelectors.length; i++) {
+            if (statSelectors[i].id !== selectorId && statSelectors[i].value == selectedStat) {
+                statSelectors[i].value = 'Empty';
+            }
+        }
+    }
+
+    async _assignStatsAndClose(html) {
+
+
+    }
+
+
 }
