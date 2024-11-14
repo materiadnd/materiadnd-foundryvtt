989b3b651bea49c69f726986cb9593767710d59b
ddipaolo <ddipaolo@gmail.com>
2024-11-14 02:32:15 -0600

    add custom armor calcs

--- src/armor.js
+++ src/armor.js
@@ -22,5 +22,48 @@ export function AddMateriaArmor() {
     CONFIG.DND5E.shieldIds.shield = 'Compendium.materia-dnd.items.Item.YMppdTgy821JBZHn';
     CONFIG.DND5E.shieldIds.tower = 'Compendium.materia-dnd.items.Item.4SwPNdzTGgKSspED';
     console.log("materia-dnd | Armor: Added armor types and shield types.");
+    console.log("materia-dnd | Armor: Adding custom armor calculations");
+    // Arcane Armor calcs for various armors (INT replaces DEX)
+    // Padded, Gambeson
+    CONFIG.DND5E.armorClasses.arcaneArmorLight1 = {
+        label: "Arcane Armor, 11 + dex",
+        formula: "11 + @abilities.int.mod"
+     };
+     // Leather
+    CONFIG.DND5E.armorClasses.arcaneArmorLight2 = {
+        label: "Arcane Armor, 11 + dex (min 1)",
+        formula: "11 + max(1, @abilities.int.mod)"
+     };
+     // Leather Jerkin
+    CONFIG.DND5E.armorClasses.arcaneArmorLight3 = {
+        label: "Arcane Armor, 12 + dex (min 1)",
+        formula: "12 + max(1, @abilities.int.mod)"
+     };
+     // Studded Leather
+    CONFIG.DND5E.armorClasses.arcaneArmorLight4 = {
+        label: "Arcane Armor, 12 + dex",
+        formula: "12 + @abilities.int.mod"
+     };
+     // Hide 
+    CONFIG.DND5E.armorClasses.arcaneArmorMedium1 = {
+        label: "Arcane Armor, 12 + dex (max 2)",
+        formula: "12 + min(2, @abilities.int.mod)"
+     };
+     // Mail Shirt 
+    CONFIG.DND5E.armorClasses.arcaneArmorMedium2 = {
+        label: "Arcane Armor, 13 + dex (max 2)",
+        formula: "13 + min(2, @abilities.int.mod)"
+     };
+     // Scale Mail, Lamellar, Hauberk, Breastplate
+    CONFIG.DND5E.armorClasses.arcaneArmorMedium3 = {
+        label: "Arcane Armor, 14 + dex (max 2)",
+        formula: "14 + min(2, @abilities.int.mod)"
+     };
+     // Brigandine
+    CONFIG.DND5E.armorClasses.arcaneArmorMedium4 = {
+        label: "Arcane Armor, 15 + dex (max 2)",
+        formula: "15 + min(2, @abilities.int.mod)"
+     };
+    console.log("materia-dnd | Armor: Added custom armor calculations");
     console.log("materia-dnd | Armor: Completed setup.");
 }
