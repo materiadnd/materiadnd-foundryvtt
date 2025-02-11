e8ced091fc10b4585f3724205fa12359d4049d23
ddipaolo <ddipaolo@gmail.com>
2025-02-10 23:45:42 -0600

    5th-level spells: should be most things but some summons

--- utils/updateSources.py
+++ utils/updateSources.py
@@ -1,28 +1,43 @@
 import os
 import json
 
-DIR_ROOT = 'packs\\_source\\spells\\4th-level-spells'
-FILES_TO_IGNORE = [ '_folder.json' ]
+DIR_ROOT = "packs\\_source\\spells\\5th-level-spells"
+FILES_TO_IGNORE = ["_folder.json"]
+
 
 def updateJsonFiles(rootDir, func):
-    for root, dirs, files, in os.walk(rootDir):
-        for file in [f for f in files if os.path.splitext(f)[1] == '.json' and f not in FILES_TO_IGNORE]:
+    for (
+        root,
+        dirs,
+        files,
+    ) in os.walk(rootDir):
+        for file in [
+            f
+            for f in files
+            if os.path.splitext(f)[1] == ".json" and f not in FILES_TO_IGNORE
+        ]:
             fullPath = os.path.join(root, file)
             func(fullPath)
+
 
 def addOrUpdateSource(filePath):
     updated = False
     jsonObj = None
-    with open(filePath, 'r') as jsonFile:
+    with open(filePath, "r") as jsonFile:
         jsonObj = json.load(jsonFile)
-        if 'system' not in jsonObj:
+        if "system" not in jsonObj:
             return  # should never happen I think
-        jsonObj['system']['source'] = {'rules': '2024', 'revision': 1, 'book': 'Materia'}
+        jsonObj["system"]["source"] = {
+            "rules": "2024",
+            "revision": 1,
+            "book": "Materia",
+        }
         updated = True
     if updated and jsonObj is not None:
-        with open(filePath, 'w') as updatedFile:
+        with open(filePath, "w") as updatedFile:
             json.dump(jsonObj, updatedFile, indent=2)
-            print(f'Updated file: {filePath}')
+            print(f"Updated file: {filePath}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     updateJsonFiles(DIR_ROOT, addOrUpdateSource)
