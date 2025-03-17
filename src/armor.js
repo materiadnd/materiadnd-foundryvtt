5271a663213e389f515dc5586e19be7713e0b017
ddipaolo <ddipaolo@gmail.com>
2025-03-17 09:27:34 -0500

    update more doc IDs

--- src/armor.js
+++ src/armor.js
@@ -1,67 +1,67 @@
 export function AddMateriaArmor() {
     console.log("materia-dnd | Armor: Adding custom armor and shield types.");
-    CONFIG.DND5E.armorIds.breastplate = 'Compendium.materia-dnd.items.Item.X7nnIdC081gjZe30';
-    CONFIG.DND5E.armorIds.brigandine = 'Compendium.materia-dnd.items.Item.2CzwK7NTkGYPGQVg';
-    CONFIG.DND5E.armorIds.chainmail = 'Compendium.materia-dnd.items.Item.39xChOi2TDLpCsqH';
-    CONFIG.DND5E.armorIds.cuirass = 'Compendium.materia-dnd.items.Item.M0f5u7FLt9ierGbz';
-    CONFIG.DND5E.armorIds.gambeson = 'Compendium.materia-dnd.items.Item.3mC7XVd9nDuogp91';
-    CONFIG.DND5E.armorIds.halfplate = 'Compendium.materia-dnd.items.Item.IpWm4bv45PqJzYZi';
-    CONFIG.DND5E.armorIds.hauberk = 'Compendium.materia-dnd.items.Item.EsZRntTx7vgRfEh1';
-    CONFIG.DND5E.armorIds.hide = 'Compendium.materia-dnd.items.Item.SvAfqxMUDyyWyFJQ';
-    CONFIG.DND5E.armorIds.jerkin = 'Compendium.materia-dnd.items.Item.bKmiINDMl8LjgPfm';
-    CONFIG.DND5E.armorIds.lamellar = 'Compendium.materia-dnd.items.Item.6swObZ50ac9T81Ht';
-    CONFIG.DND5E.armorIds.leather = 'Compendium.materia-dnd.items.Item.EPJDDYc60u9ARf2D';
-    CONFIG.DND5E.armorIds.mailshirt = 'Compendium.materia-dnd.items.Item.u2OhBq9qi8b08CU5';
-    CONFIG.DND5E.armorIds.padded = 'Compendium.materia-dnd.items.Item.nohkOiIXBX4ONISe';
-    CONFIG.DND5E.armorIds.plate = 'Compendium.materia-dnd.items.Item.SrH5qKzOiVV4Opxr';
-    CONFIG.DND5E.armorIds.ringmail = 'Compendium.materia-dnd.items.Item.ixdQKoSObyZMYPTY';
-    CONFIG.DND5E.armorIds.scalemail = 'Compendium.materia-dnd.items.Item.SMsv6uHDL4quUjLs';
-    CONFIG.DND5E.armorIds.splint = 'Compendium.materia-dnd.items.Item.yyy7xYloJS1YNaqd';
-    CONFIG.DND5E.armorIds.studded = 'Compendium.materia-dnd.items.Item.6RB6SbCsqgl9m2cP';
-    CONFIG.DND5E.shieldIds.buckler = 'Compendium.materia-dnd.items.Item.v9puBCt5FEyZokks';
-    CONFIG.DND5E.shieldIds.shield = 'Compendium.materia-dnd.items.Item.YMppdTgy821JBZHn';
-    CONFIG.DND5E.shieldIds.tower = 'Compendium.materia-dnd.items.Item.4SwPNdzTGgKSspED';
+    CONFIG.DND5E.armorIds.breastplate = 'Compendium.materia-dnd.items.Item.matIteBreastplat';
+    CONFIG.DND5E.armorIds.brigandine = 'Compendium.materia-dnd.items.Item.matIteBrigandine';
+    CONFIG.DND5E.armorIds.chainmail = 'Compendium.materia-dnd.items.Item.matIteChainMail0';
+    CONFIG.DND5E.armorIds.cuirass = 'Compendium.materia-dnd.items.Item.matIteCuirass000';
+    CONFIG.DND5E.armorIds.gambeson = 'Compendium.materia-dnd.items.Item.matIteGambeson00';
+    CONFIG.DND5E.armorIds.halfplate = 'Compendium.materia-dnd.items.Item.matIteCuirass000';
+    CONFIG.DND5E.armorIds.hauberk = 'Compendium.materia-dnd.items.Item.matIteHauberk000';
+    CONFIG.DND5E.armorIds.hide = 'Compendium.materia-dnd.items.Item.matIteHideArmor0';
+    CONFIG.DND5E.armorIds.jerkin = 'Compendium.materia-dnd.items.Item.matIteLeatherJer';
+    CONFIG.DND5E.armorIds.lamellar = 'Compendium.materia-dnd.items.Item.matIteLamellarAr';
+    CONFIG.DND5E.armorIds.leather = 'Compendium.materia-dnd.items.Item.matIteLeatherArm';
+    CONFIG.DND5E.armorIds.mailshirt = 'Compendium.materia-dnd.items.Item.matIteMailShirt0';
+    CONFIG.DND5E.armorIds.padded = 'Compendium.materia-dnd.items.Item.matItePaddedArmo';
+    CONFIG.DND5E.armorIds.plate = 'Compendium.materia-dnd.items.Item.matItePlateArmor';
+    CONFIG.DND5E.armorIds.ringmail = 'Compendium.materia-dnd.items.Item.matIteRingMail00';
+    CONFIG.DND5E.armorIds.scalemail = 'Compendium.materia-dnd.items.Item.matIteScaleMail0';
+    CONFIG.DND5E.armorIds.splint = 'Compendium.materia-dnd.items.Item.matIteSplintArmo';
+    CONFIG.DND5E.armorIds.studded = 'Compendium.materia-dnd.items.Item.matIteStuddedLea';
+    CONFIG.DND5E.shieldIds.buckler = 'Compendium.materia-dnd.items.Item.matIteBuckler000';
+    CONFIG.DND5E.shieldIds.shield = 'Compendium.materia-dnd.items.Item.matIteShield0000';
+    CONFIG.DND5E.shieldIds.tower = 'Compendium.materia-dnd.items.Item.matIteShieldTowe';
     console.log("materia-dnd | Armor: Added armor types and shield types.");
     console.log("materia-dnd | Armor: Adding custom armor calculations");
     // Arcane Armor calcs for various armors (INT replaces DEX)
     // Padded, Gambeson
     CONFIG.DND5E.armorClasses.arcaneArmorLight1 = {
-        label: "Arcane Armor, 11 + dex",
+        label: "Arcane Armor, 11 + int",
         formula: "11 + @abilities.int.mod"
      };
      // Leather
     CONFIG.DND5E.armorClasses.arcaneArmorLight2 = {
-        label: "Arcane Armor, 11 + dex (min 1)",
+        label: "Arcane Armor, 11 + int (min 1)",
         formula: "11 + max(1, @abilities.int.mod)"
      };
      // Leather Jerkin
     CONFIG.DND5E.armorClasses.arcaneArmorLight3 = {
-        label: "Arcane Armor, 12 + dex (min 1)",
+        label: "Arcane Armor, 12 + int (min 1)",
         formula: "12 + max(1, @abilities.int.mod)"
      };
      // Studded Leather
     CONFIG.DND5E.armorClasses.arcaneArmorLight4 = {
-        label: "Arcane Armor, 12 + dex",
+        label: "Arcane Armor, 12 + int",
         formula: "12 + @abilities.int.mod"
      };
      // Hide 
     CONFIG.DND5E.armorClasses.arcaneArmorMedium1 = {
-        label: "Arcane Armor, 12 + dex (max 2)",
+        label: "Arcane Armor, 12 + int (max 2)",
         formula: "12 + min(2, @abilities.int.mod)"
      };
      // Mail Shirt 
     CONFIG.DND5E.armorClasses.arcaneArmorMedium2 = {
-        label: "Arcane Armor, 13 + dex (max 2)",
+        label: "Arcane Armor, 13 + int (max 2)",
         formula: "13 + min(2, @abilities.int.mod)"
      };
      // Scale Mail, Lamellar, Hauberk, Breastplate
     CONFIG.DND5E.armorClasses.arcaneArmorMedium3 = {
-        label: "Arcane Armor, 14 + dex (max 2)",
+        label: "Arcane Armor, 14 + int (max 2)",
         formula: "14 + min(2, @abilities.int.mod)"
      };
      // Brigandine
     CONFIG.DND5E.armorClasses.arcaneArmorMedium4 = {
-        label: "Arcane Armor, 15 + dex (max 2)",
+        label: "Arcane Armor, 15 + int (max 2)",
         formula: "15 + min(2, @abilities.int.mod)"
      };
     console.log("materia-dnd | Armor: Added custom armor calculations");
--- src/conditions.js
+++ src/conditions.js
@@ -1,23 +1,23 @@
 export function AddMateriaConditions() {
     console.log("materia-dnd | Conditions: Adding custom conditions and status effects.")
     // Conditions
-    CONFIG.DND5E.conditionTypes.dazed = { 'icon': 'modules/materia-dnd/icons/statuses/noun-dazed.svg', 'label': 'Dazed', reference: 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.Sc9jlItT38o8N70h'};
-    CONFIG.DND5E.conditionTypes.possessed = { 'icon': 'modules/materia-dnd/icons/statuses/noun-ghost.svg', 'label': 'Possessed', reference: 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.zJHQBtD33KkJJunM', statuses: ['incapacitated'] };
-    CONFIG.DND5E.conditionTypes.weakened = { 'icon': 'modules/materia-dnd/icons/statuses/noun-weakness.svg', 'label': 'Weakened', reference: 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.AEqHmIZbN8B1DxAJ' };
+    CONFIG.DND5E.conditionTypes.dazed = { 'icon': 'modules/materia-dnd/icons/statuses/noun-dazed.svg', 'label': 'Dazed', reference: 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondDazed0'};
+    CONFIG.DND5E.conditionTypes.possessed = { 'icon': 'modules/materia-dnd/icons/statuses/noun-ghost.svg', 'label': 'Possessed', reference: 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondPosses', statuses: ['incapacitated'] };
+    CONFIG.DND5E.conditionTypes.weakened = { 'icon': 'modules/materia-dnd/icons/statuses/noun-weakness.svg', 'label': 'Weakened', reference: 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondWeaken' };
     console.log("materia-dnd | Conditions: Added three custom conditions.")
     // Status effects
-    CONFIG.statusEffects.push({'id': 'dazed', 'name': 'Dazed', 'icon': 'modules/materia-dnd/icons/statuses/noun-dazed.svg', 'label': 'Dazed', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.Sc9jlItT38o8N70h'});
-    CONFIG.statusEffects.push({'id': 'possessed', 'name': 'Possessed', 'icon': 'modules/materia-dnd/icons/statuses/noun-ghost.svg', 'label': 'Possessed', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.zJHQBtD33KkJJunM', 'statuses': ['incapacitated']});
-    CONFIG.statusEffects.push({'id': 'weakened', 'name': 'Weakened', 'icon': 'modules/materia-dnd/icons/statuses/noun-weakness.svg', 'label': 'Weakened', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.AEqHmIZbN8B1DxAJ'});
+    CONFIG.statusEffects.push({'id': 'dazed', 'name': 'Dazed', 'icon': 'modules/materia-dnd/icons/statuses/noun-dazed.svg', 'label': 'Dazed', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondDazed0'});
+    CONFIG.statusEffects.push({'id': 'possessed', 'name': 'Possessed', 'icon': 'modules/materia-dnd/icons/statuses/noun-ghost.svg', 'label': 'Possessed', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondPosses', 'statuses': ['incapacitated']});
+    CONFIG.statusEffects.push({'id': 'weakened', 'name': 'Weakened', 'icon': 'modules/materia-dnd/icons/statuses/noun-weakness.svg', 'label': 'Weakened', 'reference': 'Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondWeaken'});
     console.log("materia-dnd | Conditions: Added three status effects.")
     // Overwriting exhaustion
-    CONFIG.DND5E.conditionTypes.exhaustion.reference = "Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.9XAWvL8gnCtdez6U";
+    CONFIG.DND5E.conditionTypes.exhaustion.reference = "Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondExhaus";
     delete CONFIG.DND5E.conditionTypes.exhaustion.reduction;
     const exh = CONFIG.statusEffects.find(e => e.id === "exhaustion");
     foundry.utils.mergeObject(exh, CONFIG.DND5E.conditionTypes.exhaustion, { insertKeys: false });
     console.log("materia-dnd | Conditions: Overwrote existing exhaustion with Materia version.");
     // Overwriting prone
-    CONFIG.DND5E.conditionTypes.prone.reference = "Compendium.materia-dnd.rules.JournalEntry.MpEv2KTNEY6lLelV.JournalEntryPage.LaLO0fpw458OrsI6";
+    CONFIG.DND5E.conditionTypes.prone.reference = "Compendium.materia-dnd.rules.JournalEntry.matJrnConditions.JournalEntryPage.matJrnCondProne0";
     const prone = CONFIG.statusEffects.find( e => e.id === "prone" );
     foundry.utils.mergeObject(prone, CONFIG.DND5E.conditionTypes.prone, { insertKeys: false });
     console.log("materia-dnd | Conditions: Overwrote existing prone with Materia version.");
